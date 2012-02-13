769d42648db6de17b19cdea232eff8b6d8346d21
Steven Koch <stvkoch@gmail.com>
2012-02-13 14:22:34 +0000

    add placeholders features

--- Modules/Widget/widget.php
+++ Modules/Widget/widget.php
@@ -9,27 +9,16 @@ class Widget extends Inoves_Modules
 	//adiciona rotas
 	static function setup()
 	{
-		Inoves_Routes::add('Widget::begin');//all
-
-		
-/*		Inoves_Routes::add('Menu::menuLeft');//all
-		
-		Inoves_Routes::add('Menu::menuContent', array('only'=>'menu/slug'));
-		Inoves_Routes::add('Menu::homeContent', array('only'=>'/'));		
-		//Inoves_View::setLayout( 'Core/partial/layout.phtml' );
-		*/
-		//todas as rotas, except todas as actions do controllers car
-		//Inoves_Routes::add('Menu::all');
-		//Inoves_Routes::add('Menu::menuItem', array('except'=>array('/')));
-		//Inoves_Routes::add('Menu::title', array('only'=>array('ola/')));
+		Inoves_Routes::add('Widget::begin', array('except'=>'/'));//all
 	}
 	
 	
 	public function begin()
 	{
 		//facebb
-		$partial = new Inoves_Partial('Widget/partials/menu/widget.phtml');
-		Inoves_View::add_partial('sidebar', $partial);
+		$partial = new Inoves_Partial('Widget/partials/widget.phtml');
+		$partial->widget_name = 'Hello Word';
+		Inoves_View::add_partial('widgets', $partial);
 	}
 	
 	
